b913af2bdaf02746067f5167fc7492a23b93b42c
meili <meiliham1@gmail.com>
2021-02-15 12:29:40 -0500

    commit tree 63de18ebc9b9177fe8f4205154283c77e6abe0d3

---
+++ lib/dog.rb
@@ -0,0 +1,6 @@
+class Dog #creating a Dog clas
+end
+
+    fido = Dog.new #make new instance using .new method to create the dog with own ID
+    snoopy = Dog.new #make new instance using .new method to create the dog with own ID
+    lassie = Dog.new  #make new instance using .new method to create the dog with own ID
